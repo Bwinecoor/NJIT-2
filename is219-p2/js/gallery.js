19de19d95e5d91e37560d19a89e1c2736c6f8c6b
Brandon Winecoor <brandonwinecoor@gmail.com>
2024-02-06 13:48:44 -0500

    2/6/2024 Commit

--- is219-p2/js/gallery.js
+++ is219-p2/js/gallery.js
@@ -33,10 +33,15 @@ function animate() {
 /************* DO NOT TOUCH CODE ABOVE THIS LINE ***************/
 
 function swapPhoto() {
-	//Add code here to access the #slideShow element.
-	//Access the img element and replace its source
-	//with a new image from your images array which is loaded 
-	//from the JSON string
+	if (mCurrentIndex >= mJson.images.length()) {
+		mCurrentIndex = 0;
+	} else {
+		console.log('Error')
+	}
+	if (mCurrentIndex < 0) {
+		mCurrentIndex.length() = mImages.img[-1];
+	}
+	
 	console.log('swap photo');
 }
 
