3ffbb2908a12821f336183b5163994cd73d9e9ea
Bwinecoor <123968562+Bwinecoor@users.noreply.github.com>
2024-02-22 00:44:31 -0500

    Add files via upload

--- is219-p2/js/gallery.js
+++ is219-p2/js/gallery.js
@@ -1,15 +1,15 @@
 // requestAnim shim layer by Paul Irish
-    window.requestAnimFrame = (function(){
-      return  window.requestAnimationFrame       || 
-              window.webkitRequestAnimationFrame || 
-              window.mozRequestAnimationFrame    || 
-              window.oRequestAnimationFrame      || 
-              window.msRequestAnimationFrame     || 
-              function(/* function */ callback, /* DOMElement */ element){
-                window.setTimeout(callback, 1000 / 60);
-              };
-    })();
-  
+window.requestAnimFrame = (function () {
+	return window.requestAnimationFrame ||
+		window.webkitRequestAnimationFrame ||
+		window.mozRequestAnimationFrame ||
+		window.oRequestAnimationFrame ||
+		window.msRequestAnimationFrame ||
+		function (/* function */ callback, /* DOMElement */ element) {
+			window.setTimeout(callback, 1000 / 60);
+		};
+})();
+
 
 // example code from mr doob : http://mrdoob.com/lab/javascript/requestanimationframe/
 
@@ -18,7 +18,7 @@ animate();
 var mLastFrameTime = 0;
 var mWaitTime = 5000; //time in ms
 function animate() {
-    requestAnimFrame( animate );
+	requestAnimFrame(animate);
 	var currentTime = new Date().getTime();
 	if (mLastFrameTime === 0) {
 		mLastFrameTime = currentTime;
@@ -44,7 +44,7 @@ function swapPhoto() {
 
 	let photoElement = document.getElementById('photo')
 	photoElement.src = mImages[mCurrentIndex].img
-	
+
 	let location = document.getElementsByClassName('location')[0];
 	let description = document.getElementsByClassName('description')[0];
 	let date = document.getElementsByClassName('date')[0];
@@ -55,7 +55,7 @@ function swapPhoto() {
 	date.innerHTML = "Date: " + mImages[mCurrentIndex].date;
 	mLastFrameTime = 0;
 	mCurrentIndex += 1;
-	
+
 	console.log('swap photo');
 }
 
@@ -79,22 +79,22 @@ var mUrl = 'extras.json';
 //You can optionally use the following function as your event callback for loading the source of Images from your json data (for HTMLImageObject).
 //@param A GalleryImage object. Use this method for an event handler for loading a gallery Image object (optional).
 function makeGalleryImageOnloadCallback(galleryImage) {
-	return function(e) {
+	return function (e) {
 		galleryImage.img = e.target;
 		mImages.push(galleryImage);
 	}
 }
 
-$(document).ready( function() {
-	
+$(document).ready(function () {
+
 	// This initially hides the photos' metadata information
 	// $('.details').eq(0).hide();
 	fetchJSON();
-	
+
 });
 
-window.addEventListener('load', function() {
-	
+window.addEventListener('load', function () {
+
 	console.log('window loaded');
 
 }, false);
@@ -125,16 +125,52 @@ function fetchJSON() {
 				new GalleryImage(image.imgLocation, image.description, image.date, image.imgPath)
 				let galleryImage = new GalleryImage(image.imgLocation, image.description, image.date, image.imgPath);
 				mImages.push(galleryImage)
-		}
+			}
 		} else {
 			console.log("We connected to the server, but it returned on error.");
+		}
+
 	}
-	
-}
-mRequest.open("GET", mUrl)
-mRequest.send()
+	mRequest.open("GET", mUrl)
+	mRequest.send()
 }
 
-function rot90(img) {
-    img.id = "rot90";
+
+
+
+$(document).ready(function () {
+
+	$(".moreIndicator").on("click", function () {
+		if ($(".moreIndicator").hasClass("rot90")) {
+			$(".moreIndicator").removeClass('rot90').addClass("rot270")
+		} else {
+			$(".moreIndicator").removeClass("rot270").addClass("rot90")
+		}
+		$('.details').slideToggle();
+	});
+	
+	$("#prevPhoto").on("click", function () {
+		mCurrentIndex = mCurrentIndex - 2;
+		swapPhoto()
+	});
+	$("#nextPhoto").on("click", function () {
+		mCurrentIndex++
+		swapPhoto()
+	});
+	$('#nextPhoto').position({
+		of: '#nav',
+		my: 'right',
+		at: 'right'
+	});
+});
+
+function iterateJSON(mJson) {
+	for (var x = 0; x < mJson.images.length; x++) {
+		mImages[x].location = mJson.images[x].imgLocation;
+		mImages[x].date = mJson.images[x].date;
+		mImages[x].img = mJson.images[x].imgPath;
+		mImages[x] = new galleryImage();
+		mImages[x].description = mJson.images[x].description;
+		
+	}
 }
